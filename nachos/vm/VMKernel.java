a8419304ca2a28d3857a65a732bb0a208350aea9
anh010 <anh010@ucsd.edu>
2023-06-08 20:12:48 -0700

    overwritten prev commit, page pinning needs tests

--- nachos/vm/VMKernel.java
+++ nachos/vm/VMKernel.java
@@ -24,12 +24,16 @@ public class VMKernel extends UserKernel {
 	 */
 	public void initialize(String[] args) {
 		super.initialize(args);
+		updatePPMapLock = new Lock();
+		cvLock = new Lock();
+		waitForUnPin = new Condition(cvLock);
+		initializePpnPinMap();
 		freeSpnList = new LinkedList<Integer>();
 		freeSPNLock = new Lock();
 		updateCurrSPNLock = new Lock();
-		invertedPageTable = new HashMap<Integer, Entry<Integer, Integer>>();
-		vpnToSpnMap = new HashMap<Integer, Integer>();
-		swapFile = ThreadedKernel.fileSystem.open("swapFile", true);
+		invertedPageTable = new HashMap<Integer, Entry<Integer,Integer>>();
+		vpnToSpnMap = new HashMap<Integer,Integer>();
+		swapFile =  ThreadedKernel.fileSystem.open("swapFile", true);
 	}
 
 	/**
@@ -62,18 +66,28 @@ public class VMKernel extends UserKernel {
 
 	// data structure that represents an inverted page table, mapping PPNs (key)
 	// to a user processes' PID and VPN (value)
-	public static HashMap<Integer, Entry<Integer, Integer>> invertedPageTable;
-	// swap file
+	public static HashMap<Integer, Entry<Integer,Integer>> invertedPageTable;
+	// swap file 
 	public static OpenFile swapFile;
 
 	// data structure to hold vpn to spn mappings
 	public static HashMap<Integer, Integer> vpnToSpnMap;
+
+	// map physical pages to if pinned
+	public static HashMap<Integer, Boolean> ppnPinMap;
 
 	// declare a lock for free spn editing
 	private static Lock freeSPNLock;
 
 	// declare a lock for updating the current spn
 	private static Lock updateCurrSPNLock;
+	
+	// declare a lock for updating the ppnPin map
+	private static Lock updatePPMapLock;
+
+	// initializing condition variable 
+	private static Lock cvLock;
+	protected static Condition waitForUnPin;
 
 	// declare a freeSpnList, which is used for SPNS for our swap file
 	private static LinkedList<Integer> freeSpnList;
@@ -120,7 +134,6 @@ public class VMKernel extends UserKernel {
 
 	/**
 	 * this method just returns the number of free spns in our freeSpn list
-	 * 
 	 * @return size of the free spn list
 	 */
 	public static int getNumOfFreeSPNS() {
@@ -130,14 +143,131 @@ public class VMKernel extends UserKernel {
 	/**
 	 * this method will add a certain amount of spns to the spnlist
 	 */
-	public static void addMoreSPNSToList() {
+	public static void addMoreSPNSToList(){
 		// allocate a total of 100 additional spns
-		for (int i = currentSPN; i < currentSPN + NUM_OF_SPNS_TO_ALLOCATE; i++) {
+		for(int i = currentSPN; i < currentSPN + NUM_OF_SPNS_TO_ALLOCATE; i++){
 			deallocateSPN(i);
 		}
-		// increment the spn count
 		updateCurrSPNLock.acquire();
 		currentSPN += NUM_OF_SPNS_TO_ALLOCATE;
 		updateCurrSPNLock.release();
+	}
+
+	/**
+	 * pin a page to the ppnPinMap
+	 * 
+	 * @param ppn physical page to pin
+	 */
+	public static boolean pin(int ppn, int pID) {
+		updatePPMapLock.acquire();
+		// check so a process can't pin another process's page
+		if(pID != invertedPageTable.get(ppn).getKey()) {
+			updatePPMapLock.release();
+			return false;
+		}
+		// check that a valid page is being pinned (already exists in hashmap)
+		if(ppnPinMap.get(ppn) == null) {
+			updatePPMapLock.release();
+			return false;
+		}
+		ppnPinMap.put(ppn, true);
+		updatePPMapLock.release();
+		return true;
+	}
+	
+	/**
+	 * unpin a page by setting its value to false
+	 * 
+	 * @param ppn physical page to unpin
+	 * @return true on success, false on failure
+	 */
+	public static boolean unpin(int ppn, int pID) {
+		updatePPMapLock.acquire();
+		// need to check if current process is owner of pinned page 
+		if (pID != invertedPageTable.get(ppn).getKey()){
+			updatePPMapLock.release();
+			return false;			
+		}
+		// check if the ppn exists within the hashmap
+		if(ppnPinMap.get(ppn) == null) {
+			updatePPMapLock.release();
+			return false;
+		}
+		boolean ifAllPinned = checkAllPagesPinned();
+		ppnPinMap.put(ppn, false);
+		if(ifAllPinned) {
+			waitForUnPin.wake();
+		}
+		updatePPMapLock.release();
+		return true;
+	}
+	/**
+	 * Iterate through the hashmap and check if all pages are pinned
+	 * 
+	 * @return true of all pages pinned, false if at least one page is unpinned
+	 */
+	public static boolean checkAllPagesPinned(){
+		// if the ppnPin map is empty, no pages are pinned
+		if(ppnPinMap.isEmpty()){
+			return false;
+		}
+		// else if it is non empty, iterate through all pages and check if at least one of them is unpinned
+		for (Iterator<Integer> keys = ppnPinMap.keySet().iterator(); keys.hasNext();) {
+			// get the current key, a ppn
+			Integer	 ppn = keys.next();
+			// get the current value, boolean if pinned
+			Boolean isPinned = ppnPinMap.get(ppn);
+			// if any value is false (page is unpinned) then return false
+			if (!isPinned) {
+				return false;
+			}
+		}
+		// other wise every page is pinned
+		return true;		
+	}
+
+	/**
+	 * This method initializes the ppn to pin map
+	 */
+	public static void initializePpnPinMap() {
+		updatePPMapLock.acquire();
+		// make a new ppn to pin map
+		ppnPinMap = new HashMap<Integer, Boolean>();
+		// get the number of physical pages
+		int numPhysPages = Machine.processor().getNumPhysPages();
+		// initialize the ppn to pin mapping to have all pages unpinned
+		for (int index = 0; index < numPhysPages; index++) {
+			// add the page to the map, and set it as unpinned
+			ppnPinMap.put(index, false);
+		}
+		updatePPMapLock.release();
+	}
+
+	/**
+	 * this method checks if the specified ppn is pinned or not
+	 * @param ppn 
+	 * @return true if the ppn is pinned, else false if not
+	 */
+	public static boolean isPinned(int ppn) {
+		// if the ppn is null, then return false
+		if(ppnPinMap.get(ppn) == null){
+			// return false because it does not exist
+			return false;
+		}
+		// return the pin status od the specified ppn
+		return ppnPinMap.get(ppn);
+	}
+
+	/**
+	 * This method prints the current ppn to pin mappings (debugging purposes)
+	 */
+	public static void printPpnPinMap() {
+		// get the number of physical pages
+		int numPhysPages = Machine.processor().getNumPhysPages();
+		System.out.println("------PPN PIN MAP -------");
+		for (int index = 0; index < numPhysPages; index++) {
+			// print out each entry
+			System.out.println("ppn: " + index + "| isPinned: " + ppnPinMap.get(index));
+		}
 	}
 }
